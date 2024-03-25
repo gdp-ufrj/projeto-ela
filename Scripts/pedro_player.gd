3ce4b05133a02fa82d9c9d32d7f48d5ec49b9edd
IgorDelfino <igordelfinobarbosa56@gmail.com>
2024-03-25 20:24:06 -0300

    player now is stopped while in dialogue

--- Scripts/pedro_player.gd
+++ Scripts/pedro_player.gd
@@ -7,11 +7,17 @@ extends CharacterBody2D
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 
+var should_walk : bool = true
+
 func _ready():
 	update_animation_parameters(starting_direction)
+	Dialogic.signal_event.connect(_on_dialoc_signal)
 
 func _physics_process(_delta):
 	# getting the input direction
+	if(!should_walk):
+		return
+	
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
@@ -38,3 +44,9 @@ func pick_new_animation_state():
 		state_machine.travel("Walk")
 	else:
 		state_machine.travel("Idle")
+		
+func _on_dialoc_signal(argument : String):
+	if (argument == 'dialogue_started'):
+		should_walk = false
+	if (argument == 'dialogue_ended'):
+		should_walk = true
